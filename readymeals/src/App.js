717b90f8e124dc909a98b16deeb8d176ca34b013
Kevin Ton-that <tonthat.kevin@gmail.com>
2022-11-27 21:01:23 -0800

    Cart and Cart Modal

--- readymeals/src/App.js
+++ readymeals/src/App.js
@@ -4,10 +4,12 @@ import Hero from './Components/Layout/Hero';
 import About from './Components/Layout/About';
 import Card from './Components/Menu/MealItem';
 import WeeklyMeals from './Components/Menu/WeeklyMeals';
+import Cart from './Components/Cart/Cart';
 
 function App() {
   return <div>
     <Navigation />
+    <Cart />
     <Hero />
     <About />
     <WeeklyMeals />
